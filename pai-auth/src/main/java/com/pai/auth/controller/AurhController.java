9bec5b520e1063ed2cbb14d45365d674e634f07c
Dongmingming <1193217032@qq.com>
2021-03-22 17:18:21 +0800

    gateWay集成swagger2

---
+++ pai-auth/src/main/java/com/pai/auth/controller/AurhController.java
@@ -0,0 +1,22 @@
+package com.pai.auth.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 认证模块
+ *
+ * @author Chill
+ */
+@RestController
+//@AllArgsConstructor
+//@Api(value = "用户授权认证", tags = "授权接口")
+public class AurhController {
+
+
+
+
+
+
+
+
+}
