34fd40faf333b5108e969078303f46347ee2b173
Dongmingming <1193217032@qq.com>
2021-04-09 22:05:27 +0800

    pai-auth开发

--- pai-modules/src/main/java/com/pai/modules/controller/UserController.java
+++ pai-modules/src/main/java/com/pai/modules/controller/UserController.java
@@ -1,15 +1,30 @@
 package com.pai.modules.controller;
 
+import com.pai.api.domain.SysUser;
+import com.pai.api.vo.LoginUserVO;
+import com.pai.common.core.domain.R;
+import com.pai.common.core.utils.SecurityUtils;
+import com.pai.common.core.utils.StringUtils;
+import com.pai.modules.service.UserService;
+import org.springframework.web.bind.annotation.PathVariable;
+
 /**
  * 用户相关信息
  * @创建人 dmm
  */
 public class UserController {
 
+    private UserService userService;
     /**
      * 获取当前用户信息
      */
-    //public R<LoginUser>
+    public R<LoginUserVO> info(@PathVariable("username") String username){
+        SysUser sysUser = userService.selectUserByUserName(username);
+        if(StringUtils.isNull(sysUser)){
+            return R.fail("用户名或密码错误");
+        }
+        return null;
+    }
 
 
 
