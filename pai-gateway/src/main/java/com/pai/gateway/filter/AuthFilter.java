e001039d781988a405f5b3d4ee3f758766b53bce
Dongmingming <1193217032@qq.com>
2021-04-15 21:33:58 +0800

    pai-auth开发

---
+++ pai-gateway/src/main/java/com/pai/gateway/filter/AuthFilter.java
@@ -0,0 +1,28 @@
+package com.pai.gateway.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @创建人 dmm
+ */
+public class AuthFilter implements GlobalFilter, Ordered {
+
+    private static final Logger logger = LoggerFactory.getLogger(AuthFilter.class);
+
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        return null;
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
