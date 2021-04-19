98c8a7e65f338e0f7287b7c99f0f08531458ae07
Dongmingming <1193217032@qq.com>
2021-04-19 21:10:56 +0800

    pai-gateway开发

--- pai-gateway/src/main/java/com/pai/gateway/filter/AuthFilter.java
+++ pai-gateway/src/main/java/com/pai/gateway/filter/AuthFilter.java
@@ -1,12 +1,18 @@
 package com.pai.gateway.filter;
 
+import com.pai.common.redis.service.RedisService;
+import com.pai.gateway.config.properties.IgnoreWhiteProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import javax.annotation.Resource;
 
 /**
  * @创建人 dmm
@@ -15,9 +21,25 @@ public class AuthFilter implements GlobalFilter, Ordered {
 
     private static final Logger logger = LoggerFactory.getLogger(AuthFilter.class);
 
+    @Autowired
+    private IgnoreWhiteProperties ignoreWhiteProperties;
+
+    @Resource(name = "StringRedisTemplate")
+    private ValueOperations<String,String> soap;
+
+    @Autowired
+    private RedisService redisService;
 
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        String url = exchange.getRequest().getURI().getPath();
+        //if(StringUtils.)
+
+
+
+
+
         return null;
     }
 
