d1e3e689b6fe75e3cb492ffbaa8da471131a6cca
Dongmingming <1193217032@qq.com>
2021-04-26 21:43:47 +0800

    pai-gateway开发

---
+++ pai-gateway/src/main/java/com/pai/gateway/handler/SentinelFallbackHandler.java
@@ -0,0 +1,42 @@
+package com.pai.gateway.handler;
+
+import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.GatewayCallbackManager;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebExceptionHandler;
+import reactor.core.publisher.Mono;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @创建人 dmm
+ */
+public class SentinelFallbackHandler implements WebExceptionHandler {
+
+    private Mono<Void> writeResponse(ServerResponse response, ServerWebExchange exchange) {
+        ServerHttpResponse serverHttpResponse = exchange.getResponse();
+        serverHttpResponse.getHeaders().add("Content-Type", "application/json;charset=UTF-8");
+        byte[] datas = "{\"code\":429,\"msg\":\"请求超过最大数，请稍后再试\"}".getBytes(StandardCharsets.UTF_8);
+        DataBuffer buffer = serverHttpResponse.bufferFactory().wrap(datas);
+        return serverHttpResponse.writeWith(Mono.just(buffer));
+    }
+
+
+    @Override
+    public Mono<Void> handle(ServerWebExchange serverWebExchange, Throwable throwable) {
+        if (serverWebExchange.getResponse().isCommitted()) {
+            return Mono.error(throwable);
+        }
+        if (!BlockException.isBlockException(throwable)) {
+            return Mono.error(throwable);
+        }
+        return handleBlockedRequest(serverWebExchange, throwable).flatMap(response -> writeResponse(response, serverWebExchange));
+    }
+
+    private Mono<ServerResponse> handleBlockedRequest(ServerWebExchange exchange, Throwable throwable) {
+        return GatewayCallbackManager.getBlockHandler().handleRequest(exchange, throwable);
+    }
+}
