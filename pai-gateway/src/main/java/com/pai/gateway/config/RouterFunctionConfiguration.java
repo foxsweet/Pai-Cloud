912a6aa70442081aee702bae98e90e6c3b59f7be
Dongmingming <1193217032@qq.com>
2021-04-27 21:30:25 +0800

    pai-gateway开发

---
+++ pai-gateway/src/main/java/com/pai/gateway/config/RouterFunctionConfiguration.java
@@ -0,0 +1,7 @@
+package com.pai.gateway.config;
+
+/**
+ * @创建人 dmm
+ */
+public class RouterFunctionConfiguration {
+}
--- pai-modules/src/main/java/com/pai/modules/mapper/RoleMapper.java
+++ pai-modules/src/main/java/com/pai/modules/mapper/RoleMapper.java
@@ -18,4 +18,5 @@ public interface RoleMapper {
      */
     public List<SysRole> selectRolePermissionByUserId(Long userId);
 
+
 }
